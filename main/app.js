a0f4c72bfa21a9169e4afabb64bd1528bbbe1e1f
leandronunesdev <nunes.pessoal@gmail.com>
2022-03-07 20:26:07 -0300

    127. Putting It All Together: Deck Of Cards

--- main/app.js
+++ main/app.js
@@ -1,24 +1,36 @@
-const annoyer = {
-  phrases: [
-    'literally',
-    'cray cray',
-    "I can't even",
-    'Totes!',
-    'YOLO',
-    "Can't Stop, Won't Stop",
-  ],
-  pickPhrase() {
-    const { phrases } = this;
-    const idx = Math.floor(Math.random() * phrases.length);
-    return phrases[idx];
+function makeDeck() {
+  const deck = [];
+  const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+  const values = '2,3,4,5,6,7,8,9,10,J,Q,K,A';
+  for (let value of values.split(',')) {
+    for (let suit of suits) {
+      deck.push({ value, suit });
+    }
+  }
+  return deck;
+}
+
+function drawCard(deck) {
+  return deck.pop();
+}
+
+// const myDeck = makeDeck();
+// const card1 = drawCard(myDeck);
+
+const myDeck = {
+  deck: [],
+  suits: ['clubs', 'spades', 'hearts', 'diamonds'],
+  values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+  initializeDeck() {
+    const { suits, values, deck } = this;
+    for (let value of values.split(',')) {
+      for (let suit of suits) {
+        deck.push({ value, suit });
+      }
+    }
+    // return deck;
   },
-  start() {
-    this.timerId = setInterval(() => {
-      console.log(this.pickPhrase());
-    }, 3000);
-  },
-  stop() {
-    clearInterval(this.timerId);
-    console.log('PHEW');
+  drawCard() {
+    return this.deck.pop();
   },
 };
