ff1bf41e232551e2065f5b7f6874e99cae27a437
leandronunesdev <nunes.pessoal@gmail.com>
2022-03-18 14:53:55 -0300

    Section 14: Twisting the DOM to Our Will!

---
+++ main/lessons/DOMMethods.js
@@ -0,0 +1,43 @@
+// const allLis = document.querySelectorAll('li');
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+
+// // for (let i = 0; i < allLis.length; i++) {
+// //   allLis[i].innerText = 'champions';
+// // }
+
+// // for (let li of allLis) {
+// //   li.innerHTML = 'We are <b> the champions</b>';
+// // }
+
+// // for (let i = 0; i < allLis.length; i++) {
+// //   allLis[i].innerText = 'champions';
+// // }
+
+// // allLis.forEach((li, i) => {
+// //   const color = colors[i];
+// //   li.style.color = color;
+// // });
+
+// const todo = document.querySelector('#todos .todo');
+
+// todo.style.color = 'gray';
+// todo.style.textDecoration = 'line-through';
+// todo.style.opacity = '50%';
+
+// const newImg = document.createElement('img');
+
+// newImg.src =
+//   'https://imagens.brasil.elpais.com/resizer/KaUXB1Zj22WiU-T2CH6aBpnyGy4=/980x735/arc-anglerfish-eu-central-1-prod-prisa.s3.amazonaws.com/public/EIJ2SYEL44H3DWJAKG6QHI3UKU.jpg';
+
+// newImg.style.width = '300px';
+
+// document.body.appendChild(newImg);
+
+// // ('https://www.youtube.com/watch?v=vNXu4iqEeMc');
+
+// const newLink = document.createElement('a');
+// newLink.innerText = 'Cats compilation video';
+// newLink.href = 'https://www.youtube.com/watch?v=vNXu4iqEeMc';
+
+// const firstP = document.querySelector('p');
+// firstP.appendChild(newLink);
